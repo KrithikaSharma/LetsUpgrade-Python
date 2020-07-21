3c7b296a1f7c2047e9801c8993b4e715f5d203e6
KrithikaSharma <68143338+KrithikaSharma@users.noreply.github.com>
2020-07-21 23:00:46 +0530

    Add files via upload

---
+++ AssignmentDay1.py.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Python")
+
+
+# In[2]:
+
+
+print("LetsUpgrade Python")
+
+
+# In[3]:
+
+
+print(""" triple quotes""")
+
+
+# In[4]:
+
+
+print('you're amazing')
+
+
+# In[6]:
+
+
+print("you're amazing")
+print('you\'re amazing')
+
+
+# In[7]:
+
+
+print("good\tevening")
+print("good\nevening")
+
+
+# In[8]:
+
+
+print("\a this is used for alarm sound")
+
+
+# In[14]:
+
+
+subject="Python";
+batch=6
+duration="90mins"
+print("subject is",subject," batch number is",batch," of duration",duration)
+print("subject is %s batch number is %d of duration %s",(subject,batch,duration))
+print("subject is %10s",subject)
+
+
+# In[15]:
+
+
+a=b=55
+
+
+# In[16]:
+
+
+a
+
+
+# In[17]:
+
+
+b
+
+
+# In[18]:
+
+
+id(a)
+
+
+# In[19]:
+
+
+id(b)
+
+
+# In[20]:
+
+
+del a
+
+
+# In[21]:
+
+
+a
+
+
+# operators:
+# 1. Arithmetic(few not all...leaving rest because they are easy)
+
+# In[23]:
+
+
+5**2
+
+
+# In[24]:
+
+
+5*2
+
+
+# In[25]:
+
+
+5//2
+
+
+# 2. Comparision operators
+
+# In[26]:
+
+
+x=7
+y=9
+x==y
+
+
+# In[27]:
+
+
+x<y
+
+
+# In[28]:
+
+
+x>y
+
+
+# In[29]:
+
+
+x<=y
+
+
+# In[32]:
+
+
+x>=y
+
+
+# In[33]:
+
+
+x!=y
+
+
+# 3.Bitwise operators
+
+# In[36]:
+
+
+x|y
+
+
+# In[37]:
+
+
+x&y
+
+
+# In[38]:
+
+
+~x
+
+
+# 4. Logical operators
+
+# In[39]:
+
+
+5>3 and 7>4
+
+
+# In[40]:
+
+
+3>5 or 7>4
+
+
+# 5.Assignment operators 
+# 
+
+# In[52]:
+
+
+a=5
+a
+
+
+# In[53]:
+
+
+a+=3
+a
+
+
+# In[54]:
+
+
+a-=2
+a
+
+
+# In[55]:
+
+
+a*=2
+a
+
+
+# 6. Membership operator
+
+# In[59]:
+
+
+str="LetsUpgrade"
+L not in str
+
+
+# 7. Identity operators
+
+# In[60]:
+
+
+p=67
+q=67
+p is q
+
+
+# In[61]:
+
+
+z=1.5
+y=1.5
+x is not y
+
+
+# In[62]:
+
+
+e=888
+f=888
+e is y
+
+
+# In[ ]:
+
+
+
+
